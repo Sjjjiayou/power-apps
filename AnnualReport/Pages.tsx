6776225dbfe21aee25b37984d00f2a12797924ec
Song, Jingjing [MEDCN NON J&J] <JSong81@ITS.JNJ.com>
2023-10-11 17:42:31 +0800

    feat:pcf可以在js里面使用ajax调用flow，往dataverse新增一条数据

--- AnnualReport/Pages.tsx
+++ AnnualReport/Pages.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { PageProps } from "./type";
+import html2canvas from "html2canvas";
 // import { Howl } from "howler";
 
 export const Pages = React.memo((props: PageProps) => {
-  const { width, height, columns, records, auditLogSet } = props;
+  const { width, height, columns, records } = props;
+  let httpRequest;
   console.log("columns", columns);
   console.log("records", records);
 
@@ -26,34 +28,53 @@ export const Pages = React.memo((props: PageProps) => {
     "https://charm123.oss-cn-beijing.aliyuncs.com/OSS/www/first-page.png",
   ];
 
-  const handleClick = React.useCallback(() => {
-    console.log("44444444")
-    const auditLog = auditLogSet.records;
-
-    console.log("auditLog", auditLog);
-    console.log("auditLogSet-in", auditLogSet)
-    if (Object.keys(auditLog).length > 0) {
-      console.log(
-        "getColumnInfo",
-        // @ts-ignore
-        auditLog["1d85ca34-e434-ed11-9daf-0017fa020932"].getColumnInfo(
-          "AppName"
-        )
+  const handleClick = React.useCallback(async () => {
+    console.log("22222222211111");
+    const dom = document.getElementById("image-set");
+    if (dom) {
+      const imageBase64 = await html2canvas(dom, { useCORS: true }).then(
+        function (canvas) {
+          console.log(canvas);
+          const outputBase = canvas
+            .toDataURL("image/jpeg")
+            .replace("image/jpeg", "image/octet-stream");
+          return outputBase;
+        }
       );
-      auditLog["1d85ca34-e434-ed11-9daf-0017fa020932"]
-        // @ts-ignore
-        .setValue("jjmc_appname", "测试应用");
-      auditLog["1d85ca34-e434-ed11-9daf-0017fa020932"]
-        // @ts-ignore
-        .save()
-        .then((res) => {
-          console.log("res", res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      console.log(imageBase64);
+      const link = document.createElement("a");
+      link.href = imageBase64;
+      link.download = "download.png";
+      link.click();
     }
-  }, [auditLogSet]);
+
+    const url =
+      "https://prod-05.chinaeast2.logic.azure.cn:443/workflows/5886fa13499649c2afd234b9c3c79f6f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=efMCSpfEhpWf0PA4u1oCZLt7zWjlN6r0A38ChB-2RHI";
+
+    httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+      alert("放弃了 :( 不能创建 XMLHTTP 实例");
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open("POST", url);
+    httpRequest.send();
+  }, []);
+
+  const alertContents = () => {
+    try {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          alert(httpRequest.responseText);
+        } else {
+          alert("请求遇到了问题。");
+        }
+      }
+    } catch (e: any) {
+      console.log("e", e)
+      alert(`遇到了 exception：${e.description}`);
+    }
+  };
 
   // const sound = new Howl({
   //   src: [
